e738d03ba60cf5a4c6cf355c5ca2f89dc9834423
Mordekai_HD <evgeni.kap@list.ru>
2023-08-30 16:32:10 +0300

    SecondArrayWithIF

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿// Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк,
-//  длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, 
+//  длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры,
 //  либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями,
 //   лучше обойтись исключительно массивами.
 
@@ -8,7 +8,20 @@
 // [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
 // [“Russia”, “Denmark”, “Kazan”] → []
 
-string[] array1 = new string[6] {"123", "Russia", "Hello", "world", ":-)", "-2"};
+void SecondArrayWithIF(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length <= 3)
+        {
+            array2[count] = array1[i];
+            count++;
+        }
+    }
+}
+
+string[] array1 = new string[7]{ "123", "Russia", "Hello", "world", ":-)", "-2", "computer science"};
 string[] array2 = new string[array1.Length];
 SecondArrayWithIF(array1, array2);
 PrintArray(array2);
